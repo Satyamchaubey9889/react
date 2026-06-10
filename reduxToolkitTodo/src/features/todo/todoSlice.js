816aff42eaa0d40e446b6b722b127910c86222f4
Satyamchaubey9889 <Satyamchaubey9565@gmail.com>
2026-06-10 23:36:01 +0530

    Implement edit and update functionality for todos

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
-import {createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "Hello world"}]
+    todos: [{ id: 1, text: "Hello world" }]
 }
 
 
@@ -12,27 +12,24 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid(), 
+                id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
         updateTodo: (state, action) => {
-            const {id, text} = action.payload
+            const { id, text } = action.payload
             const existingTodo = state.todos.find((todo) => todo.id === id)
-            if(existingTodo){
+            if (existingTodo) {
                 existingTodo.text = text
             }
-        },
-        deleteTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         }
     }
 })
 
-export const {addTodo, removeTodo, updateTodo, deleteTodo} = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions
 
 export default todoSlice.reducer
